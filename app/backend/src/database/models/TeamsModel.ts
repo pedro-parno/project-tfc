b8027d10ed1b709d927cbe8a687adf93665cdfae
pedro-parno <pedroparno@gmail.com>
2024-04-10 23:08:27 -0300

    implementing teams model and migration

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,33 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeams extends Model<InferAttributes<SequelizeTeams>,
+InferCreationAttributes<SequelizeTeams>> {
+  declare id: number;
+  declare teamName: string;
+}
+
+SequelizeTeams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default SequelizeTeams;
